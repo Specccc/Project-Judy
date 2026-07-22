c7cd7b3c5b24c406698b036d0b573d9c1249d85e
juandre <156788655+Specccc@users.noreply.github.com>
2026-07-22 16:11:01 +0200

    Implement async database connection and initialization
    Added asynchronous database connection and initialization functions.

--- database.py
+++ database.py
@@ -1,1 +1,17 @@
+import aiosqlite
+from pathlib import Path
 
+DATABASE_PATH = Path("database/judy.db")
+
+
+async def connect():
+    DATABASE_PATH.parent.mkdir(exist_ok=True)
+
+    db = await aiosqlite.connect(DATABASE_PATH)
+    return db
+
+
+async def initialize():
+    db = await connect()
+    await db.commit()
+    await db.close()

b76ba6c1c61e457ee02e41c64db96301425641ed
juandre <156788655+Specccc@users.noreply.github.com>
2026-07-22 16:32:34 +0200

    Update log_service.py

--- log_service.py
+++ log_service.py
@@ -1,1 +1,49 @@
+import discord
+from datetime import datetime
 
+from config import (
+    COLOR_SUCCESS,
+    COLOR_WARNING,
+    COLOR_ERROR,
+    COLOR_INFO
+)
+
+
+def build_success(title: str, description: str):
+    embed = discord.Embed(
+        title=title,
+        description=description,
+        color=COLOR_SUCCESS,
+        timestamp=datetime.utcnow()
+    )
+    return embed
+
+
+def build_warning(title: str, description: str):
+    embed = discord.Embed(
+        title=title,
+        description=description,
+        color=COLOR_WARNING,
+        timestamp=datetime.utcnow()
+    )
+    return embed
+
+
+def build_error(title: str, description: str):
+    embed = discord.Embed(
+        title=title,
+        description=description,
+        color=COLOR_ERROR,
+        timestamp=datetime.utcnow()
+    )
+    return embed
+
+
+def build_info(title: str, description: str):
+    embed = discord.Embed(
+        title=title,
+        description=description,
+        color=COLOR_INFO,
+        timestamp=datetime.utcnow()
+    )
+    return embed
